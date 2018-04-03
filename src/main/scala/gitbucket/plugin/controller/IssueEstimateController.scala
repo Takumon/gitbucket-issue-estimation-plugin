529bd4fca5b4604ab5258138f83417b1a8385ebe
takumon <inouetakumon@gmail.com>
2018-04-03 20:47:01 +0900

    #1 Issue編集画面で作業量を登録・更新・削除できるようにした。プルリク編集画面のサイドバーにはEstimationを出さないようにした。

--- src/main/scala/gitbucket/plugin/controller/IssueEstimateController.scala
+++ src/main/scala/gitbucket/plugin/controller/IssueEstimateController.scala
@@ -1,8 +1,9 @@
 package gitbucket.plugin.controller
 
 
+import gitbucket.core.api.ApiError
 import gitbucket.core.controller.ControllerBase
-import gitbucket.core.service._
+import gitbucket.core.service.{IssueCreationService, _}
 import gitbucket.core.util.StringUtil._
 import gitbucket.core.util.Implicits._
 import gitbucket.core.util.ReferrerAuthenticator
@@ -13,6 +14,10 @@ import gitbucket.plugin.service.IssueEstimateService
 class IssueEstimateController
   extends JenkinsResultCommentControllerBase
     with IssueEstimateService
+    with IssueCreationService
+    with WebHookIssueCommentService
+    with LabelsService
+    with ActivityService
     with RepositoryService
     with AccountService
     with IssuesService
@@ -25,6 +30,10 @@ class IssueEstimateController
 
 trait JenkinsResultCommentControllerBase extends ControllerBase {
   self: IssueEstimateService
+    with IssueCreationService
+    with WebHookIssueCommentService
+    with LabelsService
+    with ActivityService
     with RepositoryService
     with CommitsService
     with IssuesService
@@ -35,24 +44,86 @@ trait JenkinsResultCommentControllerBase extends ControllerBase {
 
 
 
-  get("/api/v3/repos/:owner/:repository/issues/:issueId/estimate")(referrersOnly { repository =>
+  ajaxGet("/:owner/:repository/issues/:issueId/estimate")(referrersOnly { repository =>
+    println("get")
 
     val issueId = params("issueId")
 
     if ( !isInteger(issueId)) {
-
-      notFound()
+      println("get issueId not found")
+      NotFound()
     } else {
 
+      println("get issueId found")
+
       getIssueEstimate(repository.owner, repository.name, issueId.toInt).map { e =>
+        println("get issue estimation get")
+
         org.json4s.jackson.Serialization.write(Map(
           "userName" -> e.userName,
           "repositoryName" -> e.repositoryName,
           "issueId" -> e.issueId,
           "estimate" -> e.estimate
         ))
-      } getOrElse notFound()
+      } getOrElse NotFound()
     }
   })
+
+
+  ajaxPost("/:owner/:repository/issues/:issueId/estimate")(referrersOnly { repository =>
+    println("post")
+    if (isIssueEditable(repository)) {
+
+      val userName = params("owner")
+      val repositoryName = params("repository")
+      val issueId = params("issueId")
+
+      val estimate = params("estimate")
+
+      if ( !isInteger(issueId)) {
+
+        ApiError( s"Invalid issueId = $issueId" )
+
+      } else if ( !isInteger(estimate)) {
+
+        ApiError( s"Invalid estimate = $estimate" )
+
+      } else {
+        upsertIssueEstimate(userName, repositoryName, issueId.toInt, estimate.toInt)
+
+        org.json4s.jackson.Serialization.write(Map(
+          "message" -> "updated issue estimate"
+        ))
+      }
+
+    } else Unauthorized
+
+  })
+
+
+  ajaxPost("/:owner/:repository/issues/:issueId/estimate/delete")(referrersOnly { repository =>
+    if (isIssueEditable(repository)) {
+
+      val userName = params("owner")
+      val repositoryName = params("repository")
+      val issueId = params("issueId")
+
+
+      if ( !isInteger(issueId)) {
+
+        ApiError( s"Invalid issueId = $issueId" )
+
+      } else {
+        deleteIssueEstimate(userName, repositoryName, issueId.toInt)
+
+        org.json4s.jackson.Serialization.write(Map(
+          "message" -> "deleted issue estimate"
+        ))
+      }
+
+    } else Unauthorized
+
+  })
+
 
 }
